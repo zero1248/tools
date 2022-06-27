911094c1d97257847f901d518b5647106d091bf4
zero1248 <gaoyu10010@126.com>
2022-06-27 17:00:39 +0800

    add ascii script

---
+++ cpp/print_ascii/print_ascii.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+	> File Name: print_ascii.cpp
+	> Author: Isaac
+	> Created Time: Mon 27 Jun 2022 04:44:51 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include "cstdio"
+
+using namespace std;
+
+int main() {
+
+	int i;
+	cout << "------ASCII Code------" << endl;
+	cout << "| Dec | Hex | ASCII |" << endl; 
+	for(i=32; i<128; i++) {
+		printf("  %03d | %02x  |   %c\n", i, i, i);
+	}
+
+	return 0;
+}
+
+
+
+
+
