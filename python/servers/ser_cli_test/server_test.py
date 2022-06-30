e87da025e5a1d913a54485e28e0dc2752d35f7ab
zero1248 <gaoyu10010@126.com>
2022-06-30 16:47:52 +0800

    add servers/ser_cli_test

---
+++ python/servers/ser_cli_test/server_test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from socket import *
+from time import ctime
+
+HOST = ""
+PORT = 21567
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+
+tcpSerSock = socket(AF_INET, SOCK_STREAM)
+tcpSerSock.bind(ADDR)
+tcpSerSock.listen(5)
+
+while True:
+	print ("waiting for connection...")
+	tcpCliSock, addr = tcpSerSock.accept()
+	print ("...connected from:", addr)
+
+	while True:
+		# data = tcpCliSock.recv(BUFSIZi) # client prints bytes data
+		data = tcpCliSock.recv(BUFSIZ).decode("utf-8") # client prints str data
+		if not data:
+			break
+		tcpCliSock.send(("[%s] %s"% (
+				ctime(), data)).encode("utf-8") )
+		
+	tcpCliSock.close()
+tcpSerSock.close()
+
+
+
+
+
+
+
