ea7bdb152d3ab31a99d56ba4b024c0f6aebe8203
zero1248 <gaoyu10010@126.com>
2022-07-10 22:59:19 +0800

    update print_color

--- cpp/print_color/print_color.cpp
+++ cpp/print_color/print_color.cpp
@@ -33,14 +33,14 @@ int main() {
 	printf("\033[37m This text is white. \033[0m   : \\033[37m This text is white. \\033[0m \n");
 
 	cout << endl << "\033[1m------ Background view ------\033[0m" << endl;
-	printf("\033[40m This background is black. \033[0m     : \\033[40m This text is black. \\033[0m \n");
-	printf("\033[41m This background is red. \033[0m       : \\033[41m This text is red. \\033[0m \n");
-	printf("\033[42m This background is green. \033[0m     : \\033[42m This text is green. \\033[0m \n");
-	printf("\033[43m This background is yellow. \033[0m    : \\033[43m This text is yellow. \\033[0m \n");
-	printf("\033[44m This background is blue. \033[0m      : \\033[44m This text is blue. \\033[0m \n");
-	printf("\033[45m This background is magenta. \033[0m   : \\033[45m This text is magenta. \\033[0m \n");
-	printf("\033[46m This background is cyan. \033[0m      : \\033[46m This text is cyan. \\033[0m \n");
-	printf("\033[47m This background is white. \033[0m     : \\033[47m This text is white. \\033[0m \n");
+	printf("\033[40m This background is black. \033[0m     : \\033[40m This background is black. \\033[0m \n");
+	printf("\033[41m This background is red. \033[0m       : \\033[41m This background is red. \\033[0m \n");
+	printf("\033[42m This background is green. \033[0m     : \\033[42m This background is green. \\033[0m \n");
+	printf("\033[43m This background is yellow. \033[0m    : \\033[43m This background is yellow. \\033[0m \n");
+	printf("\033[44m This background is blue. \033[0m      : \\033[44m This background is blue. \\033[0m \n");
+	printf("\033[45m This background is magenta. \033[0m   : \\033[45m This background is magenta. \\033[0m \n");
+	printf("\033[46m This background is cyan. \033[0m      : \\033[46m This background is cyan. \\033[0m \n");
+	printf("\033[47m This background is white. \033[0m     : \\033[47m This background is white. \\033[0m \n");
 
     cout << endl << "\033[1m------ Foreground view and background view ------\033[0m" << endl;
 	printf("\033[32;44m This text is green with blue background. \033[0m : \\033[32;44m This text is green with blue background. \\033[0m \n");
